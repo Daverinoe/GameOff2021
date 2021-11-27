c98044dfc0a952ba2f294836574dd497d543e280
Daverinoe <31601608+Daverinoe@users.noreply.github.com>
2021-11-27 13:26:47 +0100

    Basic attack sequence has been added
    Basic attack sequence has been added.
    Just need to add the actual beam attack and the CRT will be ready to deploy.

--- Scripts/CRT.gd
+++ Scripts/CRT.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
-export(int, 0, 200) var speed : int = 100
+export(int, 0, 200) var speed : int = 1
 export(float, 0, 1) var acceleration : float = 0.1
 
-var track_enemy : bool = false
-var move_to_last_pos : bool = true
+var attacking : bool = false
+export var track_enemy : bool = false
+var move_to_last_pos : bool = false
 var enemy : Node
 var last_pos : Vector2
 var velocity
@@ -22,18 +23,24 @@ func _process(delta: float) -> void:
 		$Sprite.flip_v = true
 	
 	if track_enemy:
-		look_at(enemy.get_global_position())
+		_track_enemy()
 		
 	
-	if move_to_last_pos:
+	if move_to_last_pos and not attacking:
 		set_velocity(last_pos)
 		velocity = move_and_slide(velocity)
+	
+	if attacking:
+		velocity = Vector2(0, 0)
+		$Attack/AnimationPlayer.play("attack")
 
 
 func _on_detectionArea_body_entered(body: Node) -> void:
 	if body.is_in_group("Friendly"):
-		track_enemy = true
 		enemy = body
+		track_enemy = true
+		_track_enemy()
+		attacking = true
 
 
 func _on_detectionArea_body_exited(body: Node) -> void:
@@ -44,3 +51,19 @@ func _on_detectionArea_body_exited(body: Node) -> void:
 
 func set_velocity(enemy_position : Vector2) -> void:
 	velocity = (enemy_position - position) * speed
+
+
+func _on_attackPause_timeout() -> void:
+	attacking = true
+
+func _stop_attacking() -> void:
+	attacking = false
+	track_enemy = true
+
+func _track_enemy() -> void:
+	look_at(enemy.get_global_position())
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "attack":
+		$Attack/attackPause.start()
